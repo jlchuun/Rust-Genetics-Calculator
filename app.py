1beab06fa99a54a335937e2463b92226a28b9fa1
jlchuun <jlchuun@github.com>
2021-02-01 20:18:23 -0800

    Fixing gui layout (WIP)

--- app.py
+++ app.py
@@ -15,14 +15,11 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=700, width=700)
 canvas.pack()
 
-frame = tk.Frame(root, bg='white')
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-genEntry = scrolledtext.ScrolledText(frame, width=40)
+genEntry = scrolledtext.ScrolledText(canvas, width=40)
 # genEntry = Entry(frame, width=30)
 genEntry.pack(padx=5, pady=5)
 
-submitGen = tk.Button(frame, text='Calculate', command=retrieve, padx=10, pady=5, fg='white', bg='gray')
+submitGen = tk.Button(canvas, text='Calculate', command=retrieve, padx=10, pady=5, fg='white', bg='gray')
 submitGen.pack()
 
 
